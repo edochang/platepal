e91eda77c4ed9e2158ea06da4bdfa0ae9c78870e
xf6717 <xfeng21@utexas.edu>
2024-04-05 12:28:39 -0500

    added bottom bar navigation in main activity

--- PlatePal/app/src/main/java/com/example/platepal/ui/CreateFragment.kt
+++ PlatePal/app/src/main/java/com/example/platepal/ui/CreateFragment.kt
@@ -1,18 +1,20 @@
 package com.example.platepal.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.platepal.R
 
-class DiscoverFragment: Fragment() {
+class CreateFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.discover_fragment, container, false)
+        return inflater.inflate(R.layout.create_fragment, container, false)
     }
+
 }
